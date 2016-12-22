c5a24749dba6b44c2d54a075cf60684d29023341
Ignacio Geli <igeli@dmcibic.local>
2016-12-22 17:13:04 -0300

    Final de cap 3 master-detail

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,25 +1,97 @@
 import { Component } from '@angular/core';
-
 export class Hero {
   id: number;
   name: string;
 }
-
-
+const HEROES: Hero[] = [
+  { id: 11, name: 'Narcoman' },
+  { id: 12, name: 'Sapuco' },
+  { id: 13, name: 'Peron!' },
+  { id: 14, name: 'Magneto' },
+  { id: 15, name: 'Wolverine' },
+  { id: 16, name: 'El hombre goma' },
+  { id: 17, name: 'La goma hombre' },
+  { id: 18, name: 'Dr Sapapa' },
+  { id: 19, name: 'Manganeta' },
+  { id: 20, name: 'PipetaMan' }
+];
 
 @Component({
   selector: 'my-app',
-  template: `	<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>
-  				<div><label>id: </label>{{hero.id}}</div>
-  				<div><label>Nombre: </label>
-  				<input [(ngModel)]="hero.name" placeholder="name"></div>
-`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>Mis Heroes:</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <div *ngIf="selectedHero">
+      <h2>Detalles de: {{selectedHero.name}}</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
+        <label>Nombre: </label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+      </div>
+    </div>
+  `,
+  styles: [`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .heroes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .heroes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .heroes li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+    .heroes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `]
 })
-
 export class AppComponent {
   title = 'La pagina de los heroes';
-  hero: Hero = {
-  id: 1,
-  name: 'Tormenta'
-};
+  heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
