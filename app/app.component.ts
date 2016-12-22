1b95995d9c43f76048243e2a65caef8d8b5225e1
Ignacio Geli <igeli@dmcibic.local>
2016-12-22 16:58:10 -0300

    fin capitulo 2 hero detail

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,25 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
+
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `	<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>
+  				<div><label>id: </label>{{hero.id}}</div>
+  				<div><label>Nombre: </label>
+  				<input [(ngModel)]="hero.name" placeholder="name"></div>
+`,
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent {
+  title = 'La pagina de los heroes';
+  hero: Hero = {
+  id: 1,
+  name: 'Tormenta'
+};
+}
