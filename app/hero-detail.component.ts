d709090b63e69548ee855d74cf719c5d422861f5
Ignacio Geli <igeli@dmcibic.local>
2016-12-27 19:52:34 -0300

    capitulo 6 routing hasta despues de agregar el boton back en hero detail

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,5 +1,13 @@
-import { Component, Input } from '@angular/core';
-import { Hero } from './hero';
+import { Component, Input, OnInit }   from '@angular/core';
+import { ActivatedRoute, Params }     from '@angular/router';
+import { Location}                    from '@angular/common';
+
+import { HeroService }                from './hero.service';
+import { Hero }                       from './hero';
+
+import 'rxjs/add/operator/switchMap';
+
+
 @Component({
   selector: 'my-hero-detail',
   template: `
@@ -10,10 +18,26 @@ import { Hero } from './hero';
         <label>name: </label>
         <input [(ngModel)]="hero.name" placeholder="name"/>
       </div>
+        <button (click)="goBack()">Back</button>
     </div>
   `
 })
-export class HeroDetailComponent {
+
+export class HeroDetailComponent implements OnInit{
   @Input()
   hero: Hero;
+  constructor(
+    private HeroService: HeroService,
+    private Route: ActivatedRoute,
+    private location: Location
+    ) {}
+  ngOnInit(): void {
+    this.Route.params
+    .switchMap((params: Params) => this.HeroService.getHero(+params['id']))
+    .subscribe(hero=> this.hero = hero);
+  }
+  goBack(): void{
+    this.location.back();
+  }
+
 }
